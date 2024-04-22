7b0863de0aa2822a3bd7df1b80db35f00590de4e
K0nradus <schmitz.m.2000@gmail.com>
2024-04-22 12:49:02 +0200

    changed B4A4.2 not finished

--- Blatt4Aufgaben/Blatt4A4.2/main.c
+++ Blatt4Aufgaben/Blatt4A4.2/main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
 int main() {
-    double A, x0 = 1.0, xn;
+    long double A, x0 = 1.0, xn;
     int i = 0;
     printf("Bitte geben sie eine Zahl ein! \n");
-    scanf(" %lf", &A);
+    scanf("%Lf", &A);
     do {
         xn = ((x0 + A/x0) / 2);
+        if((xn - x0) <= 0.000001){
+            printf("x%i = %.6Lf\n", i + 1, xn);
+            break;
+        }
         x0 = xn;
         i++;
-        printf("x%i = %lf\n", i, xn);
-
-    } while (i < 20);
+        printf("x%i = %.6Lf\n", i, xn);
+    } while (i < 100);
 
 }
 
