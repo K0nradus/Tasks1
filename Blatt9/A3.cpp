6014d99f99b89095061f095c89e2447b64754fa2
K0nradus <schmitz.m.2000@gmail.com>
2024-06-07 21:22:52 +0200

    added txt file for B9A6 tidied up B9

--- Blatt9/A3.cpp
+++ Blatt9/A3.cpp
@@ -45,15 +45,15 @@ char pop(char stack[100]){
 }
 int main(){
     char bracketString[100], Stack[100] = {'\0'};
-    int proofCounterString = 0, i = 0,counterPush = 0, counterPop = 0;
+    int validString = 0, counterPush = 0, counterPop = 0;
     char bracket;
     clear(Stack);
     printf("Bitte geben sie eine Kette mit nur diesen Zeichen ein: ( [ { } ] ):\n");
     gets(bracketString);
-    for(; bracketString[i] != '\0';i++){
+    for(int i = 0; bracketString[i] != '\0';i++){
         if(bracketString[i] != '(' && bracketString[i] != '[' && bracketString[i] != '{'
         && bracketString[i] != ')' && bracketString[i] != ']' && bracketString[i] != '}'){
-            proofCounterString++;
+            validString++;
         }else if(bracketString[i] == '(' || bracketString[i] == '[' || bracketString[i] == '{'){
             bracket = bracketString[i];
             push(bracket, Stack);
@@ -70,7 +70,7 @@ int main(){
             }
         }
     }
-    if(proofCounterString != 0 || empty(bracketString)){
+    if(validString != 0 || empty(bracketString)){
         printf("Keine guelitge Eingabe!");
     }else if(counterPop == counterPush){
         printf("Es handelt sich um eine ausgeglichene Klammerung!");
