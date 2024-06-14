f9d407273342997058d972b4f781e59d1cdf8d0a
K0nradus <schmitz.m.2000@gmail.com>
2024-06-14 13:18:01 +0200

    B10 start

---
+++ B10/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+struct datum{
+    int day;
+    enum month {jan = 1, feb, mar, apr, may, jne, jly, aug, sept, oct, nov, dec};
+    int year;
+    month month;
+};
+struct person {
+    char name[50];
+    char vorname[50];
+    datum birthday;
+};
+int main() {
+    struct person importantPerson[50];
+    struct important
+}
+
+
+
