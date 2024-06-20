7f7070e7ca0152c53349d975bb76e4b34cd1bcb2
K0nradus <schmitz.m.2000@gmail.com>
2024-06-20 15:51:33 +0200

    B10conpleteted and B11 started

---
+++ B11/safeArray.h
@@ -0,0 +1,16 @@
+#ifndef B11_SAFEARRAY_H
+#define B11_SAFEARRAY_H
+class safe_array{
+    int array[100];
+public:
+    bool setAt(unsigned int,int);
+    int getAt(unsigned int);
+    int getMin();
+    bool fill(unsigned int,unsigned int, int);
+    safe_array(int n){
+        for(int i = 0; i < 100; i++){
+            array[i] = n;
+        }
+    }
+};
+#endif //B11_SAFEARRAY_H
