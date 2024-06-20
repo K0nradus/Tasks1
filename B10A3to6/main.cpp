1c57a7d1ca24346a45283e1a55edb4b080dcb602
K0nradus <schmitz.m.2000@gmail.com>
2024-06-20 11:54:24 +0200

    tidied up B10

--- B10A3to6/main.cpp
+++ B10A3to6/main.cpp
@@ -58,7 +58,7 @@ int safe_array::getMin() {
 }*/
 int main(){
     safe_array testArray(2);
-    cout << testArray.setAt(99,1) <<endl << testArray.getMin() << endl
+    cout <<testArray.getAt(40) << endl << testArray.setAt(99,1) << endl << testArray.getMin() << endl
          << testArray.fill(50, 90, 3) << endl << testArray.getAt(60)<< endl
          << testArray.setAt(100, 5) << endl << testArray.fill(90,50,6) ;
 }
