b15c3010b111dbd20604e5e20b418dd47666a47e
K0nradus <schmitz.m.2000@gmail.com>
2024-10-21 13:58:11 +0200

    chungus

--- algoDat/Uebung4/PowerFunctionsTest.java
+++ algoDat/Uebung4/PowerFunctionsTest.java
@@ -1,25 +1,29 @@
 public class PowerFunctionsTest {
 
-    public static double avgExponent(int sumExponents, int totalExponents){
-        return (double)sumExponents/(double)totalExponents;
+    public static double avg(int sum, int total) {
+        return (double) sum / (double) total;
     }
 
-    public static double avgMultiplications(int sumMultiplications, int totalMultiplications){
-        return (double)sumMultiplications/(double)totalMultiplications;
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int totalLoops;
         int sumExponents = 0;
-        for (totalLoops = 0; totalLoops < 10000;totalLoops++){
+        for (totalLoops = 0; totalLoops < 10000; totalLoops++) {
             double randomArgument = Math.random();
-            int randomExponent = (int)(1000*randomArgument);
-            PowerFunctions.power(randomArgument,randomExponent);
+            int randomExponent = (int) (1000 * randomArgument);
+            PowerFunctions.power(randomArgument, randomExponent);
             sumExponents += randomExponent;
-            PowerFunctions.fastPower(randomArgument,randomExponent);
+            PowerFunctions.fastPower(randomArgument, randomExponent);
         }
-        System.out.println("average exponent power = "+avgExponent(sumExponents,totalLoops));
-        System.out.println("average multiplications power = "+avgMultiplications(PowerFunctions.getCountPower(),totalLoops));
-        System.out.println("average multiplications fastPower = "+avgMultiplications(PowerFunctions.getCountFastPower(),totalLoops));
+        System.out.println(
+            "average exponent power = " + avg(sumExponents, totalLoops)
+        );
+        System.out.println(
+            "average multiplications power = " +
+            avg(PowerFunctions.getCountPower(), totalLoops)
+        );
+        System.out.println(
+            "average multiplications fastPower = " +
+            avg(PowerFunctions.getCountFastPower(), totalLoops)
+        );
     }
 }
